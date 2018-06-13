3d1fe0218710f9c0a0d3c436a9b6d4f67d122985
AhmedTaha201 <ahmedtaha201@gmail.com>
2018-06-13 18:15:56 +0200

    Create Recipe object class

---
+++ app/src/main/java/com/example/bakingapp/data/Recipe.java
@@ -0,0 +1,121 @@
+package com.example.bakingapp.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Taha on 6/13/2018.
+ */
+
+public class Recipe {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("ingredients")
+    @Expose
+    private List<Ingredient> ingredients = null;
+    @SerializedName("steps")
+    @Expose
+    private List<Step> steps = null;
+    @SerializedName("servings")
+    @Expose
+    private int servings;
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public class Ingredient {
+
+        @SerializedName("quantity")
+        @Expose
+        private int quantity;
+        @SerializedName("measure")
+        @Expose
+        private String measure;
+        @SerializedName("ingredient")
+        @Expose
+        private String ingredient;
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public String getMeasure() {
+            return measure;
+        }
+
+        public String getIngredient() {
+            return ingredient;
+        }
+    }
+
+
+    public class Step {
+        @SerializedName("id")
+        @Expose
+        private int id;
+        @SerializedName("shortDescription")
+        @Expose
+        private String shortDescription;
+        @SerializedName("description")
+        @Expose
+        private String description;
+        @SerializedName("videoURL")
+        @Expose
+        private String videoURL;
+        @SerializedName("thumbnailURL")
+        @Expose
+        private String thumbnailURL;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getShortDescription() {
+            return shortDescription;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getVideoURL() {
+            return videoURL;
+        }
+
+        public String getThumbnailURL() {
+            return thumbnailURL;
+        }
+
+    }
+}
