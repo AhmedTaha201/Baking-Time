8f696fe7aa8c3f61af69f6d47880579711dc26c0
AhmedTaha201 <ahmedtaha201@gmail.com>
2018-06-16 14:06:40 +0200

    Show ingridients in recipe activity

--- app/src/main/java/com/example/bakingapp/RecipeActivity.java
+++ app/src/main/java/com/example/bakingapp/RecipeActivity.java
@@ -1,12 +1,23 @@
 package com.example.bakingapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.bakingapp.data.Recipe;
+import com.google.android.flexbox.FlexboxLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fisk.chipcloud.ChipCloud;
+import fisk.chipcloud.ChipCloudConfig;
+import fisk.chipcloud.ChipListener;
 
 /**
  * Created by Taha on 6/14/2018.
@@ -28,9 +39,31 @@ public class RecipeActivity extends AppCompatActivity {
         Intent recipeIntent = getIntent();
         if (recipeIntent != null && recipeIntent.hasExtra(RECIPE_EXTRA)) {
             mRecipe = recipeIntent.getParcelableExtra(RECIPE_EXTRA);
+            showIngridientChips(mRecipe.getIngredients());
             Log.e(LOG_TAG, "Got the recipe extra");
         } else {
             Log.e(LOG_TAG, "No extra recipe");
         }
+    }
+
+    public void showIngridientChips(List<Recipe.Ingredient> ingredients) {
+        List<String> ingridientsList = new ArrayList<>();
+        for (Recipe.Ingredient i : ingredients) {
+            String ingridientString = i.getIngredient() ;//+ " (" + String.valueOf(i.getQuantity() + " " + i.getMeasure() + ")");
+            ingridientsList.add(ingridientString);
+        }
+
+        final FlexboxLayout layout = (FlexboxLayout) findViewById(R.id.chips_group_layout);
+
+        ChipCloudConfig config = new ChipCloudConfig()
+                .selectMode(ChipCloud.SelectMode.multi)
+                .useInsetPadding(true)
+                .checkedChipColor(Color.parseColor("#ddaa00"))
+                .checkedTextColor(Color.parseColor("#ffffff"))
+                .uncheckedChipColor(Color.parseColor("#efefef"))
+                .uncheckedTextColor(Color.parseColor("#666666"));
+
+        final ChipCloud chips = new ChipCloud(this, layout, config);
+        chips.addChips(ingridientsList);
     }
 }
